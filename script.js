aea95da1820c49c02e563be13724e46e65c072cd
jud-ie19 <owuorakinyijudith@gmail.com>
2022-07-11 08:59:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let count=0;
+let countE1 =document.getElementById("count-e1");
+let saveE1=document.getElementById("save-e1")
+
+function increment(){
+    count+=1
+    countE1.innerText=count;
+
+}
+function save(){
+    countStr=count+"-";
+    saveE1.textContent+=countStr;
+    countE1.textContent=0;
+    count=0;
+
+}
